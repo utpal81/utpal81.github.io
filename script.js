67aedf6838b01315be9028813287fc3948c06a80
utpal81 <toutpal@gmail.com>
2026-06-28 00:20:52 +0530

    Latest Modification 4

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
+// Toggle dark mode
 const button = document.getElementById("themeButton");
 button.addEventListener("click", function () {
   document.body.classList.toggle("dark-mode");
+});
+
+// Scroll back to top
+const backToTop = document.getElementById("backToTop");
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 300) {
+    backToTop.style.display = "block";
+  } else {
+    backToTop.style.display = "none";
+  }
+});
+
+backToTop.addEventListener("click", function () {
+  window.scrollTo({
+    top: 0,
+
+    behavior: "smooth",
+  });
 });
